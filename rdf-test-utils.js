dd356e2fca9ee65d8b9d8de72cb7e2e9cdf3b773
bergi <bergi@axolotlfarm.org>
2015-09-25 23:52:15 +0200

    added browserify support, dropped umd support, use new JSON-LD parser module

--- rdf-test-utils.js
+++ rdf-test-utils.js
@@ -1,119 +1,88 @@
-(function (root, factory) {
-  if (typeof module !== 'undefined' && module.exports) {
-	var
-	  assert = require('assert'),
-	  jsonld = require('jsonld');
+var assert = require('assert');
+var jsonld = require('jsonld');
+var rdf = require('rdf-ext')();
+var JsonLdParser = require('rdf-parser-jsonld');
 
-	module.exports = factory(assert, jsonld, rdf);
-  } else {
-	root.rdf.testUtils = factory(root.assert, root.jsonld, root.rdf);
-  }
-})(this, function (assert, jsonld) {
-  return function (rdf) {
-	var u = {};
+var u = {};
 
-	u.compareGraph = function (a, b, callback) {
-	  if (a == null) a = rdf.createGraph();
-	  if (b == null) b = rdf.createGraph();
+u.compareGraph = function (a, b, callback) {
+  if (a == null) a = rdf.createGraph();
+  if (b == null) b = rdf.createGraph();
 
-	  rdf.serializeJsonLd(a, function (aJsonLd) {
-		jsonld.normalize(aJsonLd, {format: 'application/nquads'}, function (error, aNormalized) {
-		  rdf.serializeJsonLd(b, function (bJsonLd) {
-			jsonld.normalize(bJsonLd, {format: 'application/nquads'}, function (error, bNormalized) {
-			  callback(aNormalized == bNormalized, aNormalized, bNormalized);
-			});
-		  });
-		});
-	  });
-	};
-
-	u.p = {};
-
-    if (typeof module !== 'undefined' && module.exports) {
-      u.p.readFile = function (filename, base) {
-        return new Promise(function (resolve) {
-          resolve(require('fs').readFileSync(require('path').join(base, filename)).toString());
+  JsonLdParser.parse(a, function (aJsonLd) {
+    jsonld.normalize(aJsonLd, {format: 'application/nquads'}, function (error, aNormalized) {
+      JsonLdParser.parse(b, function (bJsonLd) {
+        jsonld.normalize(bJsonLd, {format: 'application/nquads'}, function (error, bNormalized) {
+          callback(aNormalized == bNormalized, aNormalized, bNormalized);
         });
-      };
-    } else {
-      u.p.readFile = function (filename) {
-        return new Promise(function (resolve) {
-          rdf.defaultRequest('GET', filename, {}, null, function (status, headers, content) {
-            resolve(content);
-          });
-        });
-      };
-    }
+      });
+    });
+  });
+};
 
-	u.p.parseTurtle = function (s, b) {
-	  return new Promise(function (resolve, reject) {
-		rdf.parseTurtle(s, function (g) {
-		  if (g == null)
-			reject();
-		  else
-			resolve(g);
-		}, b);
-	  });
-	};
+u.p = {};
 
-	u.p.parseJsonLd = function (s, b) {
-	  return new Promise(function (resolve, reject) {
-		rdf.parseJsonLd(s, function (g) {
-		  if (g == null)
-			reject();
-		  else
-			resolve(g);
-		}, b);
-	  });
-	};
-
-	u.p.request = function (r) {
-	  return new Promise(function (resolve, reject) {
-		r.end(function (err, res) {
-		  if (err != null)
-			reject(err);
-		  else
-			resolve(res);
-		});
-	  });
-	};
-
-	u.p.assertStatusSuccess = function (res) {
-	  return new Promise(function (resolve, reject) {
-		var success = (res.status >= 200 && res.status < 300);
-
-		assert.ok(success, 'status code != 2xx');
-
-		if (!success)
-		  reject(res);
-		else
-		  resolve(res);
-	  });
-	};
-
-	u.p.assertGraphEqual = function (a, b, m) {
-	  return new Promise(function (resolve, reject) {
-		u.compareGraph(a, b, function (equals, aNorm, bNorm) {
-		  assert.equal(aNorm, bNorm, m != null ? m : 'graph a != b');
-
-		  if (!equals)
-			reject();
-		  else
-			resolve();
-		});
-	  });
-	};
-
-	u.p.assertGraphEmpty = function (a) {
-	  return u.p.assertGraphEqual(a, rdf.createGraph(), 'graph not empty');
-	};
-
-	u.p.wait = function (t) {
-	  return new Promise(function (resolve, reject) {
-		setTimeout(resolve, t);
-	  });
-	};
-
-	return u;
+if (process.browser) {
+  u.p.readFile = function (filename) {
+    return new Promise(function (resolve) {
+      rdf.defaultRequest('GET', filename, {}, null, function (status, headers, content) {
+        resolve(content);
+      });
+    });
   };
-});
+} else {
+  u.p.readFile = function (filename, base) {
+    return new Promise(function (resolve) {
+      resolve(require('fs').readFileSync(require('path').join(base, filename)).toString());
+    });
+  };
+}
+
+u.p.request = function (r) {
+  return new Promise(function (resolve, reject) {
+    r.end(function (err, res) {
+      if (err != null)
+        reject(err);
+      else
+        resolve(res);
+    });
+  });
+};
+
+u.p.assertStatusSuccess = function (res) {
+  return new Promise(function (resolve, reject) {
+    var success = (res.status >= 200 && res.status < 300);
+
+    assert.ok(success, 'status code != 2xx');
+
+    if (!success)
+      reject(res);
+    else
+      resolve(res);
+  });
+};
+
+u.p.assertGraphEqual = function (a, b, m) {
+  return new Promise(function (resolve, reject) {
+    u.compareGraph(a, b, function (equals, aNorm, bNorm) {
+      assert.equal(aNorm, bNorm, m != null ? m : 'graph a != b');
+
+      if (!equals)
+        reject();
+      else
+        resolve();
+    });
+  });
+};
+
+u.p.assertGraphEmpty = function (a) {
+  return u.p.assertGraphEqual(a, rdf.createGraph(), 'graph not empty');
+};
+
+u.p.wait = function (t) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, t);
+  });
+};
+
+module.exports = u;
